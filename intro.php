ea9fbb5b8d2c8b0bccdc559fdb67109d48730e62
bamboovennus <bamboovennus@gmail.com>
2020-07-06 17:16:49 +0700

    3

--- intro.php
+++ intro.php
@@ -4,7 +4,7 @@
     <title>Bảng cửu chương x7</title>
 </head>
 <body>
-<img src ="tree.jpg" width="500" height="333">
+<img src ="D:\Dowload\tree.jpg" width="500" height="333">
 <?php
 for ($i =1;$i <10;$i++ )
     echo "7 x " .$i .  " = " . 7 * $i. "<br>";
